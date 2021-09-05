b0c8b92d5b774c0dac863d0828ee083d2b00dd78
apoorva <apoorva.srivastava2019@vitstudent.ac.in>
2021-09-06 01:17:10 +0530

    added hypothesis testing py

---
+++ Hypthesis Testing/HypothesisTesting.py
@@ -0,0 +1,31 @@
+import csv
+num_attributes = 6
+a = []
+
+print("\n The Given Training Data Set \n")
+with open('activity 1/ENJOYSPORT.csv', 'r') as sport:
+    reader = csv.reader(sport)
+    for row in reader:
+        a.append (row)
+        print(row)
+
+print("\n The initial value of hypothesis: ")
+hypothesis = ['0'] * num_attributes
+print(hypothesis)
+
+for j in range(0,num_attributes):
+    hypothesis[j] = a[0][j];
+print(hypothesis)
+
+print("\n Find S: Finding a Maximally Specific Hypothesis\n")
+for i in range(0,len(a)):
+    if a[i][num_attributes]=='1':
+        for j in range(0,num_attributes):
+            if a[i][j]!=hypothesis[j]:
+                hypothesis[j]='?'
+            else :
+                hypothesis[j]= a[i][j]
+    print(" For Training instance No:{0} the hypothesis is".format(i),hypothesis)
+
+print("\n The Maximally Specific Hypothesis for a given Training Examples :\n")
+print(hypothesis)
